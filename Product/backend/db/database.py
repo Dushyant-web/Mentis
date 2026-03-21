757a5e14225054bb264bf1bc7cd129565f8d202e
dushyant <dushyant@Dushyants-MacBook-Air.local>
2026-03-21 22:19:16 +0530

    backend ready,frontend ready,fronten not connected to backend,rdis redis caching done, security done ais lock,training system done fuck bhai

--- Product/backend/db/database.py
+++ Product/backend/db/database.py
@@ -1,4 +1,7 @@
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
@@ -8,7 +11,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-# Dependency (for FastAPI)
 def get_db():
     db = SessionLocal()
     try:
