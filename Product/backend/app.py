7a1f0c5377a2f4291950c72d62758991d036ea10
dushyant <dushyant@Dushyants-MacBook-Air.local>
2026-03-21 22:47:04 +0530

    backend ready,frontend ready,fronten not connected to backend,rdis redis caching done, security done ais lock,training system done fuck bhai

--- Product/backend/app.py
+++ Product/backend/app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,11 +16,6 @@ from db import models
 from routes import assessment, auth, training, dashboard
 from ml.model_loader import get_model, get_scaler
 
-import sys
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(BASE_DIR)
 
 model = get_model()
 scaler = get_scaler()
